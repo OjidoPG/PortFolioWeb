7cc008e38c8ea4cd8148992dcc1e841d51786d7b
Pascal <giraud.pascal@outlook.com>
2019-02-11 19:47:37 +0100

    Premier push

---
+++ SitePersoWeb/App_Start/BundleConfig.cs
@@ -0,0 +1,15 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace SitePersoWeb
+{
+    public class BundleConfig
+    {
+        // Pour plus d'informations sur le regroupement, visitez https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/bundles/css").Include("~/vendor/bootstrap/css/bootstrap.min.css", "~/vendor/fontawesome-free/css/all.min.css", "~/css/resume.min.css", 
+                "~/css/regExp.css"));
+        }
+    }
+}
---
+++ SitePersoWeb/Controllers/ExpProController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SitePersoWeb.Controllers
+{
+    public class ExpProController : Controller
+    {
+        public ActionResult Inf()
+        {
+            return View();
+        }
+
+        public ActionResult Dev()
+        {
+            return View();
+        }
+
+        public ActionResult Divers()
+        {
+            return View();
+        }
+    }
+}
